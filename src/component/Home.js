77df4604245dbf8763c0e162e62442e3c20a56b7
talhaunas1 <mtalha4600@gmail.com>
2023-02-15 21:33:53 +0500

    first commit

---
+++ src/component/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './Sections'
+
+const Home = () => {
+    return (
+        <Container>
+            <Section/>
+        </Container>
+    )
+}
+
+export default Home
+
+const Container = styled.div`
+height:100vh;
+// background-color:black
+
+
+`
